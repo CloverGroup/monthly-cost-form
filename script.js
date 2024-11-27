89e6b4d1946df650c00b82f083f24c7b5ded9c33
CloverGroup <knoguchi@day-clover.com>
2024-11-27 19:25:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -390,7 +390,7 @@ async function handleSubmit(event) {
 
         // Google Apps Scriptへの送信処理
         const formData = collectFormData();
-        const response = await fetch('https://script.google.com/macros/s/AKfycbw3GkOFZmyOWTUiv1zzzLKwhT3_PgGYyOVh0Fw0nhKjmCTo0YsX0Cpb2ncERmfGy-U/exec', {
+       const response = await fetch('https://script.google.com/macros/s/AKfycbw3GkOFZmyOWTUiv1zzzLKwhT3_PgGYyOVh0Fw0nhKjmCTo0YsX0Cpb2ncERmfGy-U/exec', {
             method: 'POST',
             mode: 'no-cors',
             body: JSON.stringify(formData)
